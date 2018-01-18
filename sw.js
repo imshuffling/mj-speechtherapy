c199924e022a22686c427e3ef1165178c492fecf
David Riches <hi@davidrich.es>
2018-01-18 14:55:31 +0000

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-753409725cd3bdecf252.js","a54e4685506bb0afc308cfc35970cadf"],["/app-7f6293c8cfc53ecffe8c.js","023c31b201307418cfc32a3db1b952ca"],["/app-bdda9550b376610f81b9.js","9867b3cca36a9ce330d8ddf37cdcbc60"],["/commons-43211b24d1c1daae657e.js","f4d8954c6f1b495165f6e450a696b001"],["/commons-961887d3f3b2d16d5e81.js","cc820828248811e89f9f8484955bec85"],["/index.html","818f2ab3035a596683c51dcf481517d9"],["/manifest.json","2a800e9b52731b5a0b2c7050d92dcbad"],["/offline-plugin-app-shell-fallback/index.html","6bb9351417520676e557d151ae634175"]];
+var precacheConfig = [["/mj-speechtherapy/app-753409725cd3bdecf252.js","a54e4685506bb0afc308cfc35970cadf"],["/mj-speechtherapy/app-7f6293c8cfc53ecffe8c.js","023c31b201307418cfc32a3db1b952ca"],["/mj-speechtherapy/app-bdda9550b376610f81b9.js","dbd99de7aef0ab0f4549bd2adcad403e"],["/mj-speechtherapy/commons-43211b24d1c1daae657e.js","f4d8954c6f1b495165f6e450a696b001"],["/mj-speechtherapy/commons-961887d3f3b2d16d5e81.js","181b6e5facfbec04420d89c9c2470f94"],["/mj-speechtherapy/index.html","088fb733c1e814c11c9f9e017581bd29"],["/mj-speechtherapy/manifest.json","2a800e9b52731b5a0b2c7050d92dcbad"],["/mj-speechtherapy/offline-plugin-app-shell-fallback/index.html","ebe0edf5252d8dc4a3c6b34ebe41b202"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
