33d4339c80b2702bb99d6b66759da1e9dd15a081
David Riches <hi@davidrich.es>
2018-07-06 10:32:08 +0100

    cleaning up layout, adding new favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,22 +6,17 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: "Mary Jane Speech Therapy - I offer private Speech and Language Therapy to children within the West Kent and East Sussex area.",
-        short_name: "mj-speechtherapy.co.uk",
+        short_name: "MJ",
         start_url: "/",
         background_color: "#ffffff",
         theme_color: "#6c27ae",
         display: "standalone",
         icons: [
           {
-            // Everything in /static will be copied to an equivalent
-            // directory in /public during development and build, so
-            // assuming your favicons are in /static/favicons,
-            // you can reference them here
             src: `/favicons/android-chrome-192x192.png`,
             sizes: `192x192`,
             type: `image/png`,
@@ -34,5 +29,6 @@ module.exports = {
         ],
       },
     },
+    'gatsby-plugin-offline',
   ],
 };
