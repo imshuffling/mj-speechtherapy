3a9d19c5ba0fe2dce71f7e77537e2ec415d74c82
David Riches <hi@davidrich.es>
2019-05-01 15:32:49 +0100

    updating year

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const Footer = () => (
   <footer id="Footer">
     <div className="Footer-inner">
-      <div><p>Mary Jane Iles © 2018</p></div>
+      <div><p>Mary Jane Iles © {this.getYear()}</p></div>
       <div><p><Link to='/privacy-policy'>Privacy policy</Link></p></div>
     </div>
   </footer>
